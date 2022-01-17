25b184b14344b3d1af4ae33f50bdb495cb0d37e7
jzapataherrera39 <jzapataherrera38@correo.unicordoba.edu.co>
2022-01-17 18:54:15 -0500

    Update

---
+++ src/components/contenido.vue
@@ -0,0 +1,27 @@
+<template>
+
+     <v-row no-gutters>
+         <v-col align="center">
+          <div>
+            <video controls width="80%">
+              <source src="../assets/Media/1.mp4" type="video/mp4">
+              
+            </video>
+          </div>
+         </v-col>
+       </v-row>
+      
+      
+    
+</template>
+
+<script>
+export default {
+  name: "contenido",
+  data: () => ({
+    //
+  }),
+  beforeDestroy() {
+    },
+};
+</script>
