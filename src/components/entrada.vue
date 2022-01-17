bd94972149dad4db50b169193850c039b0a02c92
jzapataherrera39 <jzapataherrera38@correo.unicordoba.edu.co>
2022-01-17 14:47:13 -0500

    Summary

--- src/components/entrada.vue
+++ src/components/entrada.vue
@@ -1,23 +1,76 @@
 <template>
 <div id="principal">
  <v-sheet elevation="6">
+  <template>
+    <div class="hidden">
+      <vs-sidebar
+        absolute
+        v-model="active"
+        open
+        >
+        <template #logo>
+          <!-- ...img logo -->
+        </template>
+        <vs-sidebar-item id="home">
+          <template #icon>
+            <i class='bx bx-home'></i>
+          </template>
+          Home
+        </vs-sidebar-item>
+        <vs-sidebar-item id="market">
+          <template #icon>
+            <i class='bx bx-grid-alt'></i>
+          </template>
+          Market Overview
+        </vs-sidebar-item>
+        <vs-sidebar-item id="Music">
+          <template #icon>
+            <i class='bx bxs-music'></i>
+          </template>
+          Music
+        </vs-sidebar-item>
+        <vs-sidebar-item id="donate">
+          <template #icon>
+            <i class='bx bxs-donate-heart' ></i>
+          </template>
+          Donate
+        </vs-sidebar-item>
+        <vs-sidebar-item id="drink">
+          <template #icon>
+            <i class='bx bx-drink'></i>
+          </template>
+          Drink
+        </vs-sidebar-item>
+        <vs-sidebar-item id="shopping">
+          <template #icon>
+            <i class='bx bxs-shopping-bags'></i>
+          </template>
+          Shopping
+        </vs-sidebar-item>
+        <vs-sidebar-item id="chat">
+          <template #icon>
+            <i class='bx bx-chat' ></i>
+          </template>
+          Chat
+        </vs-sidebar-item>
+        <template #footer>
+          <vs-row justify="space-between">
+            <vs-avatar>
+              <img src="/avatars/avatar-5.png" alt="">
+            </vs-avatar>
 
+            <vs-avatar badge-color="danger" badge-position="top-right">
+              <i class='bx bx-bell' ></i>
 
-          <v-tab :to="{ name: 'contenido' }">Contenido
-            <v-icon   x-small class="mb-5"></v-icon>
-          </v-tab>
-
-          <v-tab :to="{ name: 'actividades' }" class="text-decoration-none mx-auto px-1 tabs_box">Actividades
-            <v-icon x-large class="mb-5">fas fa-list-ol</v-icon>
-          </v-tab>
-
-        <v-tab :to="{ name: 'evaluacion' }" class="text-decoration-none mx-auto px-1 tabs_box">Evaluación
-            <v-icon x-large class="mb-5">fas fa-clipboard-check</v-icon>
-          </v-tab>
-          
-          <v-tab :to="{ name: 'creditos' }" class="text-decoration-none mx-auto px-1 tabs_box">Creditos
-            <v-icon x-large class="mb-5">fas fa-clipboard-check</v-icon>
-          </v-tab>
+              <template #badge>
+                28
+              </template>
+            </vs-avatar>
+          </vs-row>
+        </template>
+      </vs-sidebar>
+    </div>
+  </template>
     <!--   
  <v-img
       src="../assets/Ilustraciones/03.png"
