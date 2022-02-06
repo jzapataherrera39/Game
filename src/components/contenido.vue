572fc2887ea3811aaac056084de72b5c654d1211
jzapataherrera39 <jzapataherrera38@correo.unicordoba.edu.co>
2022-02-05 22:45:36 -0500

    Summary

--- src/components/contenido.vue
+++ src/components/contenido.vue
@@ -1,5 +1,6 @@
 <template>
 
+
      <v-row no-gutters>
          <v-col align="center">
           <div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,36 +1,78 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import entrada from "../components/entrada.vue";
-import Inicio from "../components/inicio"
+import Inicio from '../components/inicio'
 import contenido from "../components/contenido";
+import creditos from "../components/creditos";
+import entrada from "../components/entrada.vue";
+import actividades from"../components/actividades";
+import acti1 from "../components/h5p/actividad1";
+import acti2 from "../components/h5p/actividad2";
+import acti3 from "../components/h5p/actividad3";
+import evaluacion from"../components/evaluacion";
 Vue.use(VueRouter)
 
 const routes = [
-  {
-     path: '/',
-     name: 'inicio',
-     component: Inicio
-  },
- {
-     path: '/entrada',
-     name: 'entrada',
-     component : entrada,
-    
-    children: [
-      {
-       path: '/contenido',
-       name: 'contenido',
-       component: contenido,
-      },
-    ],
-}
-]
+     {
+        path: '/',
+        name: 'inicio',
+        component: Inicio
+     },
+    {
+        path: '/entrada',
+        name: 'entrada',
+        component : entrada,
+       
+        children: [
+         {
+          path: '/contenido',
+          name: 'contenido',
+          component: contenido,
+         },
+         {
+          path: '/actividades',
+          name: 'actividades',
+          component: actividades,
+          children: [
+            {
+                 path: 'acti1',
+                 name: 'acti1',
+                 component: acti1,
+              },
+              {
+                path: 'acti2',
+                name: 'acti2',
+                component: acti2,
+             },
+             {
+              path: 'acti3',
+              name: 'acti3',
+              component: acti3,
+           }  
 
 
-  const router = new VueRouter({
+        ],
+        },
+        {
+          path: '/evaluacion',
+          name: 'evaluacion',
+          component: evaluacion,
+          children: [
+
+        ],
+          },
+        {
+         path: '/creditos',
+         name: 'creditos',
+         component: creditos,
+         },
+        ],
+    }
+ ]
+
+const router = new VueRouter({
     mode: "history",
   base: process.env.BASE_URL,
     routes
 })
-
+ 
 export default router
